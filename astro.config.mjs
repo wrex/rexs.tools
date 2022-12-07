15d02ef4824544d33f09a4372a9197ee5ec3b8c6
Rex Walters <653208+wrex@users.noreply.github.com>
2022-12-07 10:58:11 -0800

    add mdx, remove markdown config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,10 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
-// import mdx from '@astrojs/mdx'
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-blog-template.netlify.app",
-  // integrations: [mdx(), svelte()],
-  integrations: [svelte()],
-  markdown: {
-    shikiConfig: {
-      theme: "nord",
-    },
-    rehypePlugins: [
-      [
-        "rehype-external-links",
-        {
-          target: "_blank",
-        },
-      ],
-    ],
-  },
+  integrations: [mdx(), svelte()],
+  // integrations: [svelte()],
 });
