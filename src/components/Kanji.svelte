f0d7a8b29f7772791b05b6ee7502e59131df7825
Rex Walters <653208+wrex@users.noreply.github.com>
2022-12-08 14:42:22 -0800

    use pico tooltips for kanji!

--- src/components/Kanji.svelte
+++ src/components/Kanji.svelte
@@ -5,39 +5,6 @@
   import { furiganaType } from "../stores/furiganaType";
 </script>
 
-<span class="container">
-  <span class="furigana">
-    {$furiganaType === "kana" ? furigana : romaji}
-  </span>
-  <span class="kanji">
-    <slot />
-  </span>
+<span data-tooltip={$furiganaType === "kana" ? furigana: romaji}>
+  <slot />
 </span>
-
-<style>
-  .container {
-    position: relative;
-    display: inline-block;
-    width: auto;
-  }
-
-  .furigana {
-    font-size: 50%;
-    line-height: 1;
-    position: absolute;
-    top: -0.5em;
-    width: 100%;
-    min-width: max-content;
-    text-align: center;
-  }
-  
-  @media (hover: hover) {
-    .furigana {
-      visibility: hidden;
-    }
-    .container:hover .furigana {
-      visibility: visible;
-    }
-  }
-
-</style>
