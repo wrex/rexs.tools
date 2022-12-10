f16085ece1ecb27b0136c7fde17d2f957532f303
Rex Walters <653208+wrex@users.noreply.github.com>
2022-12-10 12:44:25 -0800

    add some images, more blogstuff

---
+++ src/components/BlogList.svelte
@@ -0,0 +1,29 @@
+<script type="ts">
+  export let posts = [];
+  const rowSize = 2;
+  
+  // Turn flat list of posts into array of rows, with rowSize elements in each row
+  let rows = [];
+  for (let i = 0; i < posts.length; i += rowSize) {
+    rows.push(posts.slice(i, i + rowSize));
+  }
+</script>
+
+{#each rows as rowPosts, i}
+  <section class="grid">
+    {#each rowPosts as post}
+    <article>
+      <hgroup>
+        <h4><a href={post.url}>{post.frontmatter.title}</a></h4>
+        <date>{post.frontmatter.pubDate.slice(0,10)}</date>
+      </hgroup>
+    </article>
+    {/each}
+  </section>
+{/each}
+
+<style>
+  date {
+    color: var(--secondary);
+  }
+</style>
