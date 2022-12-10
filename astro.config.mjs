05ef00e00ff7bd091de35c04a3b3c5fa36fdf496
Rex Walters <653208+wrex@users.noreply.github.com>
2022-12-09 17:34:48 -0800

    astro-images, embed youtube, real content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import mdx from "@astrojs/mdx";
+import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-blog-template.netlify.app",
-  integrations: [mdx(), svelte()],
+  integrations: [astroImageTools, mdx(), svelte()],
   // integrations: [svelte()],
+  vite: {
+    ssr: {
+      noExternal: ["astroImageTools"],
+    },
+  },
 });
