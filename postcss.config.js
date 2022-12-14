c715619c875de886531f844ecc48f36b74bf4a1c
Rex Walters <653208+wrex@users.noreply.github.com>
2022-12-13 16:35:01 -0800

    install openprops postcss-jit-props

---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+// postcss.config.js
+const postcssJitProps = require('postcss-jit-props');
+const OpenProps = require('open-props');
+
+module.exports = {
+  plugins: [
+    postcssJitProps({
+      files: [
+        require('open-props/open-props.min.css'),
+      ]
+    }),
+  ]
+}
