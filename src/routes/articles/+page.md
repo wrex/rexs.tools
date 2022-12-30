3220f91601f87181aa046ff6d6404f5bda317a27
Rex Walters <653208+wrex@users.noreply.github.com>
2022-12-29 19:04:34 -0800

    install mdsvex, articles placeholder

---
+++ src/routes/articles/+page.md
@@ -0,0 +1,6 @@
+---
+---
+
+# Articles
+
+This section will eventually contain long-form content organized like a book.
--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,12 @@
+import { mdsvex } from 'mdsvex';
+import mdsvexConfig from './mdsvex.config.js';
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: ['.svelte', ...mdsvexConfig.extensions],
+
 	kit: {
 		adapter: adapter()
 	},
@@ -10,7 +14,8 @@ const config = {
 	preprocess: [
 		preprocess({
 			postcss: true
-		})
+		}),
+		mdsvex(mdsvexConfig)
 	]
 };
 
