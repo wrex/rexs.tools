95936e7a3d580997c8d999d47609abbfaf776e51
Rex Walters <653208+wrex@users.noreply.github.com>
2022-12-30 13:03:19 -0800

    move styles to $lib

--- src/lib/stores/furiganaType.js
+++ src/lib/stores/furiganaType.js
@@ -2,9 +2,7 @@ import { writable } from 'svelte/store';
 
 export const furiganaType = writable('romaji');
 
-let furiType = '';
-
 if (typeof localStorage !== 'undefined' && localStorage.getItem('furiganaType')) {
-	furiType = localStorage.getItem('furiganaType') || 'romaji';
-	furiganaType.set(furiType);
+	let type = localStorage.getItem('furiganaType') || 'romaji';
+	furiganaType.set(type);
 }
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,14 @@
 
 <script>
   import '../app.postcss';
-  import "../styles/global.css";
+  import "$lib/styles/global.css";
 
   import Nav from './Nav.svelte';
   import Footer from './Footer.svelte';
+
+  // prerender everything by default (in this top level layout)
+  // turn this off on individual routes by settting prerender to false
+  export const prerender = true;
 </script>
 
 <!-- <Nav current={current} /> -->
